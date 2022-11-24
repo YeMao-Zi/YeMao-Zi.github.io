471304a4a394d8bba7810ff87c3d964bdf86d609
zhushengjie <1794541268@qq.com>
2022-11-24 19:15:15 +0800

    优化部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,23 @@
-
-#!/usr/bin/env sh
- 
 # 确保脚本抛出遇到的错误
 set -e
- 
+
+
+push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:zhushengjie123/zhushengjie123.github.io.git
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=gh-pages # 推送的分支
+
 # 生成静态文件
 npm run build
- 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.yourwebsite.com' > CNAME
- 
+# 进入生成的文件夹
+# docs/.vuepress/dist
+cd $dist_path
+
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
 
-# 如果你想要部署到 https://USERNAME.github.io
-# git push -f git@github.com:nan-gong/nan-gong.github.io.git master
-
-# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
- 
-git push -f git@github.com:zhushengjie123/zhushengjie123.github.io.git master:gh-pages  
-# 这句需要配置自己的仓库地址,除过这句代码,其他均复制粘贴即可
- 
 cd -
-# rm -rf docs/.vuepress/dist
+rm -rf $dist_path
