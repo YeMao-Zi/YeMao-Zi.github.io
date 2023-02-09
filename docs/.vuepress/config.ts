c1c76f49eb230f705d06728b45e180f573c1bd17
zhushengjie <1794541268@qq.com>
2023-02-09 18:20:08 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -183,11 +183,11 @@ export default defineConfig4CustomTheme<blogConfig>({
           title: "GitHub",
           link: "https://github.com/zhushengjie123",
         },
-        // {
-        //   iconClass: 'icon-erji',
-        //   title: '听音乐',
-        //   link: 'https://music.163.com/#/playlist?id=755597173',
-        // },
+        {
+          iconClass: 'icon-erji',
+          title: '听音乐',
+          link: 'https://music.163.com/#/playlist?id=755597173',
+        },
       ],
     },
     extendFrontmatter: {
@@ -239,38 +239,38 @@ export default defineConfig4CustomTheme<blogConfig>({
     //     hostname: 'https://zhushengjie123.github.io',
     //   },
     // ],
-    "vuepress-plugin-baidu-autopush", // 百度自动推送
+    // "vuepress-plugin-baidu-autopush", // 百度自动推送
 
-    [
-      "vuepress-plugin-baidu-tongji", // 百度统计
-      {
-        hm: baiduCode,
-      },
-    ],
-    // 全文搜索 已弃用：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。
-    "fulltext-search",
-
-    // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
     // [
-    //   "thirdparty-search",
+    //   "vuepress-plugin-baidu-tongji", // 百度统计
     //   {
-    //     thirdparty: [
-    //       {
-    //         title: "在MDN中搜索",
-    //         frontUrl: "https://developer.mozilla.org/zh-CN/search?q=", // 搜索链接的前面部分
-    //         behindUrl: "", // 搜索链接的后面部分，可选，默认 ''
-    //       },
-    //       {
-    //         title: "在Runoob中搜索",
-    //         frontUrl: "https://www.runoob.com/?s=",
-    //       },
-    //       {
-    //         title: "在Bing中搜索",
-    //         frontUrl: "https://cn.bing.com/search?q=",
-    //       }
-    //     ],
+    //     hm: baiduCode,
     //   },
     // ],
+    // 全文搜索 已弃用：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。
+    // "fulltext-search",
+
+    // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
+    [
+      "thirdparty-search",
+      {
+        thirdparty: [
+          {
+            title: "在MDN中搜索",
+            frontUrl: "https://developer.mozilla.org/zh-CN/search?q=", // 搜索链接的前面部分
+            behindUrl: "", // 搜索链接的后面部分，可选，默认 ''
+          },
+          {
+            title: "在Runoob中搜索",
+            frontUrl: "https://www.runoob.com/?s=",
+          },
+          {
+            title: "在Bing中搜索",
+            frontUrl: "https://cn.bing.com/search?q=",
+          }
+        ],
+      },
+    ],
 
     [
       "one-click-copy", // 代码块复制按钮
