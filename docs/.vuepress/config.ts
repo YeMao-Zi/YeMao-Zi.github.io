95ee10b50b15b259949400063cecdea883feb464
yemao <1794541268@qq.com>
2023-09-14 11:34:06 +0800

    updateBlog

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -255,20 +255,20 @@ export default defineConfig4CustomTheme<blogConfig>({
     //   },
     // ],
     // 全文搜索
-    // "fulltext-search",// 此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！
+    "fulltext-search",// 此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！
 
     // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数,只能在服务器环境，githubPages中会报错）
-    [
-      "thirdparty-search",
-      {
-        thirdparty: [
-          {
-            title: "在Bing中搜索",
-            frontUrl: "https://cn.bing.com/search?q=",
-          },
-        ],
-      },
-    ],
+    // [
+    //   "thirdparty-search",
+    //   {
+    //     thirdparty: [
+    //       {
+    //         title: "在Bing中搜索",
+    //         frontUrl: "https://cn.bing.com/search?q=",
+    //       },
+    //     ],
+    //   },
+    // ],
 
     [
       "one-click-copy", // 代码块复制按钮
